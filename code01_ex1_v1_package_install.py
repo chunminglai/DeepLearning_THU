1ca314738e867b80ee67b1fdd8c3aecb198bc1b6
Chun-Ming Lai <cmlaih@gmail.com>
2020-11-10 15:20:37 +0800

    Example Codes

---
+++ code01_ex1_v1_package_install.py
@@ -0,0 +1,25 @@
+'''
+this is not a real python code. this document
+just a quick summary about some packages that you
+should install in our class.
+'''
+1. anaconda: (please install python 3.5.6)
+https://docs.anaconda.com/anaconda/install/
+
+2. tensorflow (required):
+TensorFlow (1.9 version)
+https://www.tensorflow.org/install/
+GPU version is more complicated, so recommend CPU first.
+
+* you can also intall through anaconda, Docker, or source codes.
+
+3. Keras (required):
+https://keras.io/
+
+pip install keras (2.2)
+
+4. scikit learn (required):
+http://scikit-learn.org/stable/
+
+pip install -U scikit-learn
+
